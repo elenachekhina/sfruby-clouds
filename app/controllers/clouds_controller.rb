fc988c38a9f1a02ead19b0de05232f3a23c3aae7
Vladimir Dementyev <dementiev.vm@gmail.com>
2025-10-22 16:28:17 +0300

    + clouds infinite scroll pagination

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -1,6 +1,16 @@
 class CloudsController < ApplicationController
   def index
     @clouds = Cloud.picked.ordered
+    @clouds = @clouds.where("id < ?", params[:cursor]) if params[:cursor].present?
+    @clouds = @clouds.limit(20).to_a
+
+    @next_cursor = @clouds.last.id if @clouds.size >= 20
+
+    if turbo_frame_request?
+      render partial: "more_clouds"
+    else
+      @total_clouds = Cloud.picked.count
+    end
   end
 
   def show
