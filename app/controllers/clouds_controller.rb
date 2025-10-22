5313f57cd17f948650c0be2f132afcabad191798
Vladimir Dementyev <dementiev.vm@gmail.com>
2025-10-22 08:03:58 +0300

    - show all clouds in the gallery

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -1,12 +1,12 @@
 class CloudsController < ApplicationController
   def index
-    @clouds = Cloud.picked.ordered.limit(50)
+    @clouds = Cloud.picked.ordered
   end
 
   def show
     participant = Participant.friendly.find(params[:id])
     @picked_cloud = participant.clouds.find_by!(picked: true)
-    @clouds = Cloud.picked.ordered.limit(50)
+    @clouds = Cloud.picked.ordered
 
     render action: :index
   end
